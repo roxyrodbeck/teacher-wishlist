36613f501163c92a21b5086056c6e22f100033ee
Roxy Rodriguez-Becker <roxy.rodriguez@gmail.com>
2025-08-15 16:15:30 -0500

    edited web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\TeacherController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('teachers.create');
 });
+
+Route::get('/create', [TeacherController::class, 'create'])->name('teachers.create');
+Route::post('/teachers', [TeacherController::class, 'store'])->name('teachers.store');
+
+Route::get('/teacher/{slug}', [TeacherController::class, 'show'])->name('teachers.show');
+
