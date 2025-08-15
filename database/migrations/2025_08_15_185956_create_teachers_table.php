8ab756d3bcb393203d7d9bf2bf32a77ab442f49e
Roxy Rodriguez-Becker <roxy.rodriguez@gmail.com>
2025-08-15 14:53:36 -0500

    updated the create teachers table

---
+++ database/migrations/2025_08_15_185956_create_teachers_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('wishlist_link');
+            $table->string('paypal_email');
+            $table->string('unique_slug')->unique();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('teachers');
+    }
+};
